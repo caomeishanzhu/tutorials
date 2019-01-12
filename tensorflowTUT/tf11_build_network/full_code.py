ea702c38156e07405caa48cd1048f58e6aa0f2d0
caomeishanzhu <33675755+caomeishanzhu@users.noreply.github.com>
2019-01-12 21:01:16 +0800

    Update full_code.py

--- tensorflowTUT/tf11_build_network/full_code.py
+++ tensorflowTUT/tf11_build_network/full_code.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 def add_layer(inputs,in_size,out_size,activation_function=None):
     Weights=tf.Variable(tf.random_normal([in_size,out_size]))
@@ -12,19 +13,23 @@ def add_layer(inputs,in_size,out_size,activation_function=None):
     return outputs
 
 if __name__ == '__main__':
-    x_data=np.linspace(-1,1,10)[:,np.newaxis]
-    noise=np.random.normal(0,0.5,x_data.shape)
-    y_data=np.square(x_data)-0.5+noise
+    x_data=np.linspace(-1,1,300)[:,np.newaxis]
+    noise=np.random.normal(0,0.05,x_data.shape)
+    y_data=np.square(x_data)+noise
     xs=tf.placeholder(tf.float32,[None,1])
     ys=tf.placeholder(tf.float32,[None,1])
-    l1=add_layer(xs,1,10,activation_function=tf.nn.relu)
-    prediction=add_layer(l1,10,1,activation_function=None)
+    l1=add_layer(xs,1,300,activation_function=tf.nn.relu)
+    prediction=add_layer(l1,300,1,activation_function=None)
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]))
     train_step=tf.train.GradientDescentOptimizer(0.1).minimize(loss)
     init=tf.initialize_all_variables()
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(x_data, y_data)
+    plt.show()
     with tf.Session() as sess:
         sess.run(init)
         for i in range(1000):
             sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
             # if i%50 == 0:
-            print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+            # print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
